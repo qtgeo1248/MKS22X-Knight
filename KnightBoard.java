8b8d420ca193fbd19dd5dd02a8aa28dda277fb50
qtgeo1248 <gzhou00@stuy.edu>
2019-02-13 11:21:28 -0500

    created a boolean where it tells you whether the board is blank or not

--- KnightBoard.java
+++ KnightBoard.java
@@ -1,12 +1,14 @@
 public class KnightBoard {
 
     int[][] board;
+    boolean isBlank;
 
     public KnightBoard(int startingRows, int startingCols) {
         if (startingRows <= 0 || startingCols <= 0) {
             throw new IllegalArgumentException();
         }
         board = new int[startingRows][startingCols];
+        isBlank = true;
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board[i].length; j++) {
                 board[i][j] = 0;
@@ -16,9 +18,13 @@ public class KnightBoard {
 
     public String toString() {
         String ans = "";
-        for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board.length; j++) {
-                
+        if (isBlank) {
+
+        } else {
+            for (int i = 0; i < board.length; i++) {
+                for (int j = 0; j < board.length; j++) {
+
+                }
             }
         }
         return ans;
