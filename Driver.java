8c93c333eff899eccb77155d8092eb3f829b8400
qtgeo1248 <gzhou00@stuy.edu>
2019-02-13 11:41:34 -0500

    more test cases

--- Driver.java
+++ Driver.java
@@ -2,10 +2,16 @@ public class Driver {
     public static void main(String[] args) {
         KnightBoard test = new KnightBoard(4, 4);
         System.out.println(test + "\n");
+        test = new KnightBoard(10, 4);
+        System.out.println(test + "\n");
 
+        /*
         test.board[0][0] = 4;
         test.board[0][3] = 23;
         test.isBlank = false;
         System.out.println(test + "\n");
+        */
+
+
     }
 }
