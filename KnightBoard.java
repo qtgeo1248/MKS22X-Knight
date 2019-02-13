bcc69a6f76c4fc5f57bbf2c722d395f387378de1
qtgeo1248 <gzhou00@stuy.edu>
2019-02-13 11:15:36 -0500

    made everything in board to 0

--- KnightBoard.java
+++ KnightBoard.java
@@ -7,5 +7,10 @@ public class KnightBoard {
             throw new IllegalArgumentException();
         }
         board = new int[startingRows][startingCols];
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
+                board[i][j] = 0;
+            }
+        }
     }
 }
