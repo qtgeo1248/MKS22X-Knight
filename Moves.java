72058a8a9c85ea79d70437c0d258b0b4d978a1f5
qtgeo1248 <gzhou00@stuy.edu>
2019-02-20 08:49:16 -0500

    made "data structure" class

---
+++ Moves.java
@@ -0,0 +1,8 @@
+public class Moves {
+
+    public int[][] grid; //make private later
+
+    public Moves(int rows, int cols) { //constructor makes all the moves in rows x cols board
+        grid = new int[rows][cols];
+    }
+}
