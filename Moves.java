47fc9a60b9bb21192c7df79199534178e076afb1
qtgeo1248 <gzhou00@stuy.edu>
2019-02-20 08:52:15 -0500

    set up if statements in constructor

--- Moves.java
+++ Moves.java
@@ -4,5 +4,18 @@ public class Moves {
 
     public Moves(int rows, int cols) { //constructor makes all the moves in rows x cols board
         grid = new int[rows][cols];
+        for (int i = 0; i < grid.length; i++) {
+            if (i == 0) {
+
+            } else if (i == 1) {
+
+            } else if (i == grid.length - 1) {
+
+            } else if (i == grid.length) {
+
+            } else {
+                
+            }
+        }
     }
 }
