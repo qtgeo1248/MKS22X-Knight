731b0d78a53a855182bbeb3db58187e0198620c9
qtgeo1248 <gzhou00@stuy.edu>
2019-02-20 18:41:19 -0500

    set up for loop to finish func

--- Moves.java
+++ Moves.java
@@ -3,14 +3,23 @@ import java.util.Arrays;
 public class Moves {
 
     public int[][] grid; //make private later
+    private int rows;
+    private int cols;
 
     private int totalBeginning(int row, int col) { //function that determines number of moves
         int[] options = {2, -1, 2, 1, -2, -1, -2, 1, 1, -2, 1, 2, -1, -2, -1, 2};
         int count = 0;
+        for (int i = 0; i < options.length; i += 2) {
+            int newRow = row + options[i];
+            int newCol = col + options[i + 1];
+            if (newRow >= 0 && newRow < rows && newCol >= 0 && newCol < cols) {
+                count++;
+            }
+        }
         return count;
     }
 
-    public Moves(int rows, int cols) { //constructor makes all the moves in rows x cols board
+    public Moves(int numrows, int numcols) { //constructor makes all the moves in rows x cols board
         grid = new int[rows][cols];
         for (int i = 0; i < grid.length; i++) {
             for (int j = 0; j < grid[i].length; j++) {
