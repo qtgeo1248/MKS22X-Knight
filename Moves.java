6b6a3310e996c2ad9dab637766765558ca76bee0
qtgeo1248 <gzhou00@stuy.edu>
2019-02-20 15:21:54 -0500

    did first row of moves

--- Moves.java
+++ Moves.java
@@ -5,16 +5,24 @@ public class Moves {
     public Moves(int rows, int cols) { //constructor makes all the moves in rows x cols board
         grid = new int[rows][cols];
         for (int i = 0; i < grid.length; i++) {
-            if (i == 0) {
+            for (int j = 0; j < grid[i].length; j++) {
+                if (i == 0) {
+                    if (j == 0 || j == grid[i].length - 1) {
+                        grid[i][j] = 2; //this is corner
+                    } else if (j == 1 || j == grid[i].length - 2) {
+                        grid[i][j] = 3; //this is near corner
+                    } else {
+                        grid[i][j] = 4; //this is nonspecial edges
+                    }
+                } else if (i == 1) {
 
-            } else if (i == 1) {
+                } else if (i == grid.length - 1) {
 
-            } else if (i == grid.length - 1) {
+                } else if (i == grid.length) {
 
-            } else if (i == grid.length) {
+                } else {
 
-            } else {
-                
+                }
             }
         }
     }
