e23c3cd861d815261555417e34d474728ab8a9c7
qtgeo1248 <gzhou00@stuy.edu>
2019-02-20 21:29:06 -0500

    made test cases

--- Moves.java
+++ Moves.java
@@ -31,27 +31,28 @@ public class Moves {
     }
 
     public boolean addK(int row, int col) { //adds the knight and updates the grid.
-        if (grid[row][col] == 9) {
+        if (grid[row][col] == -1) { //-1 means the knight has been there
             return false;
         } else {
             for (int i = 0; i < OPTIONS.length; i += 2) {
                 int newRow = row + OPTIONS[i];
                 int newCol = col + OPTIONS[i + 1];
                 if (newRow >= 0 && newRow < rows && newCol >= 0 && newCol < cols) {
-                    if (grid[newRow][newCol] != 9) {
+                    if (grid[newRow][newCol] != -1) {
                         grid[newRow][newCol]--;
                     }
                 }
             }
-            grid[row][col] = 9;
+            grid[row][col] = -1;
             return true;
         }
     }
     public static void main(String[] args) {
-        Moves test = new Moves(3, 7);
+        Moves test = new Moves(3, 4);
         for (int i = 0; i < test.grid.length; i++) {
             System.out.println(Arrays.toString(test.grid[i]));
         }
         System.out.println();
+        test.addK(0, 0);
     }
 }
