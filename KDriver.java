ef13da53417994ef52f1f416832bd34f14c63e63
qtgeo1248 <gzhou00@stuy.edu>
2019-02-25 11:38:44 -0500

    put in test cases

--- KDriver.java
+++ KDriver.java
@@ -22,4 +22,9 @@ public class KDriver {
             }
         }
     }
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+            System.out.println(runTest(i));
+        }
+    }
 }
